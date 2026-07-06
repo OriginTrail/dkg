c753511add49d9094f69c2bd6bcf97c0fe4b6eb3
Jurij Skornik <jurij.skornik@gmail.com>
2026-07-06 13:03:44 +0200

    Anchor plugin context compatibility keys

--- packages/cli/test/type/plugin-api-compatibility.typecheck.ts
+++ packages/cli/test/type/plugin-api-compatibility.typecheck.ts
@@ -2,6 +2,47 @@ import type { RequestContext, RoutePlugin } from '../../src/daemon/plugin-api.js
 
 type Assert<T extends true> = T;
 type IsOptional<T, K extends keyof T> = {} extends Pick<T, K> ? true : false;
+type IsNever<T> = [T] extends [never] ? true : false;
+
+type ExpectedRequestContextKeys =
+  | 'req'
+  | 'res'
+  | 'agent'
+  | 'publisherControl'
+  | 'publisherRuntime'
+  | 'config'
+  | 'startedAt'
+  | 'dashDb'
+  | 'opWallets'
+  | 'network'
+  | 'tracker'
+  | 'memoryManager'
+  | 'bridgeAuthToken'
+  | 'nodeVersion'
+  | 'nodeCommit'
+  | 'catchupTracker'
+  | 'extractionRegistry'
+  | 'fileStore'
+  | 'extractionStatus'
+  | 'assertionImportLocks'
+  | 'vectorStore'
+  | 'embeddingProvider'
+  | 'validTokens'
+  | 'apiHost'
+  | 'apiPortRef'
+  | 'routePlugins'
+  | 'admission'
+  | 'url'
+  | 'path'
+  | 'requestIdentity'
+  | 'requestAuth'
+  | 'requestToken'
+  | 'requestAgentAddress'
+  | 'emitMemoryGraphChanged'
+  | 'emitNotification';
+
+type MissingRequestContextKeys = Exclude<ExpectedRequestContextKeys, keyof RequestContext>;
+type UnexpectedRequestContextKeys = Exclude<keyof RequestContext, ExpectedRequestContextKeys>;
 
 type RequestContextWithoutIdentity = Omit<RequestContext, 'requestIdentity'>;
 type PluginHandleContext = Parameters<RoutePlugin['handle']>[0];
